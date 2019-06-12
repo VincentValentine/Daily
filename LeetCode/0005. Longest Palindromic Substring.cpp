819bbba97a2860f5be7cb27ed232d86dbd1d4e05
VincentValentine <sephy_cy@outlook.com>
2019-06-12 16:50:19 +0800

    Update 0005. Longest Palindromic Substring.cpp

--- LeetCode/0005. Longest Palindromic Substring.cpp
+++ LeetCode/0005. Longest Palindromic Substring.cpp
@@ -13,7 +13,7 @@ public:
 
         for(i=0; i<p.size(); ++i) {
             radius[i] = maxRight>i?min(radius[2*pos-i], maxRight-i):1;
-            while(i+radius[i]<p.size() && i-radius[i]>=0 && p[i+radius[i]]==p[i-radius[i]]) ++ radius[i];
+            while(i+radius[i]<p.size() && i-radius[i]>=0 && p[i+radius[i]]==p[i-radius[i]]) ++radius[i];
             if(i+radius[i] > maxRight) {
                 maxRight = i+radius[i];
                 pos = i;
