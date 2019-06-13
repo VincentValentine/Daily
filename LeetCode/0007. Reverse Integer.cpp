6107ca027f2ea2c6fcebb9da4cbb25055889cb25
VincentValentine <sephy_cy@outlook.com>
2019-06-13 15:54:25 +0800

    Update 0007. Reverse Integer.cpp

--- LeetCode/0007. Reverse Integer.cpp
+++ LeetCode/0007. Reverse Integer.cpp
@@ -5,8 +5,8 @@ public:
         while(x) {
             pop = x%10;
             x /= 10;
-            if(ans>INT_MAX/10 ||(ans==INT_MAX/10&&pop>7)) { ans = 0; break; }
-            if(ans<INT_MIN/10 ||(ans==INT_MIN/10&&pop<-8)) { ans = 0; break; }
+            if(ans>INT_MAX/10 || (ans==INT_MAX/10&&pop>7)) { ans = 0; break; }
+            if(ans<INT_MIN/10 || (ans==INT_MIN/10&&pop<-8)) { ans = 0; break; }
             ans = ans*10+pop;
         }
 
