34aec35dca04f2d9330b3f65358e731d6f830708
VincentValentine <sephy_cy@outlook.com>
2019-07-17 11:32:11 +0800

    Create 0035. Search Insert Position.cpp

---
+++ LeetCode/0035. Search Insert Position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size(), left = 0, right = n-1, mid;
+
+        while(left <= right) {
+            mid = left+(right-left)/2;
+            if(target == nums[mid]) return mid;
+            else if(target > nums[mid]) left = mid+1;
+            else right = mid-1;
+        } 
+        return left;
+    }
+};
