1e75cb5e0ce237d17d1f61a7aeb05dac4f7c808c
VincentValentine <sephy_cy@outlook.com>
2019-07-24 10:55:33 +0800

    Update 0088. Merge Sorted Array.cpp

--- LeetCode/0088. Merge Sorted Array.cpp
+++ LeetCode/0088. Merge Sorted Array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i, p = m+n-1, pm = m-1, pn = n-1;
-        
-        while(pm>=0 && pn>=0) nums1[p--] = nums1[pm]>nums2[pn]?nums1[pm--]:nums2[pn--];
-        while(pn >= 0) nums1[p--] = nums2[pn--];
+        int i = m-1, j = n-1, k = m+n-1;
+
+        while(j >= 0) nums1[k--] = i>=0&&nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
     }
 };
