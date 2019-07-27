a65808d1bcb3595253251d9133e7877c7f4c283e
VincentValentine <sephy_cy@outlook.com>
2019-07-27 22:28:32 +0800

    Create 0094. Binary Tree Inorder Traversal.cpp

---
+++ LeetCode/0094. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        
+        inorder(root, res);
+        
+        return res;
+    }
+    
+    void inorder(TreeNode* root, vector<int> &res) {
+        if(root == nullptr) return ;
+        
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+};
