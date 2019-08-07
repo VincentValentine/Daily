fbd5f4683b9eaa2bde1b1ad8122d4d226a7ed5f3
VincentValentine <sephy_cy@outlook.com>
2019-08-07 10:26:50 +0800

    Update 0102. Binary Tree Level Order Traversal.cpp

--- LeetCode/0102. Binary Tree Level Order Traversal.cpp
+++ LeetCode/0102. Binary Tree Level Order Traversal.cpp
@@ -12,8 +12,8 @@ public:
         vector<int> temp;
         if(level == levels.size()) levels.push_back(temp);
         levels[level].push_back(root->val);
-        if(root->left != nullptr) helper(root->left, level+1);
-        if(root->right != nullptr) helper(root->right, level+1);
+        helper(root->left, level+1);
+        helper(root->right, level+1);
         return levels;
     }
 };
