fc42bc7447bc71b61ef0747f48f6f3cb5f55aee9
VincentValentine <sephy_cy@outlook.com>
2019-08-08 23:45:32 +0800

    Create 0105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

---
+++ LeetCode/0105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1);
+    }
+
+    TreeNode* helper(vector<int> preorder, int ps, int pe, vector<int>inorder, int is, int ie) {
+        if(ps > pe) return nullptr;
+
+        int i;
+        TreeNode* root = new TreeNode(preorder[ps]);
+        for(i=is; i<=ie; ++i) 
+            if(inorder[i] == root->val) break;
+        root->left = helper(preorder, ps+1, ps+i-is, inorder, is, i-1);
+        root->right = helper(preorder, ps+i-is+1, pe, inorder, i+1, ie);
+        return root;
+    }
+};
